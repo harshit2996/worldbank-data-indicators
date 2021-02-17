2aade6f4c09e7c6764b88a5dc837682ed13af20c
harshit2996 <harshitsingh2996@gmail.com>
2021-02-18 03:30:02 +0530

    Basic Application Structure

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
+def indicators(request):
+  data = [{'Indicators':'Population'}]
+  return JsonResponse(data,safe=False)
